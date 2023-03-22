78dbc3a573588926ef7870a2780eef3e63b8466a
fconiglione <faconigl@lakeheadu.ca>
2023-03-22 10:40:19 -0400

    Week 10 - Connected CSS to the view

--- src/main/java/com/example/w23comp1008lhvideogame/GameBoardController.java
+++ src/main/java/com/example/w23comp1008lhvideogame/GameBoardController.java
@@ -1,4 +1,21 @@
 package com.example.w23comp1008lhvideogame;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
 public class GameBoardController {
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private Button startButton;
+
+    @FXML
+    void startGame(ActionEvent event) {
+
+    }
+
 }
